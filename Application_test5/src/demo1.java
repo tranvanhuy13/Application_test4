e9fcf490f6089da6f2ccbe0c37c44c1bc8c6f3a7
anhtu <anhtu@tuan13042005>
2025-04-06 20:31:36 +0700

    cec

--- Application_test5/src/demo1.java
+++ Application_test5/src/demo1.java
@@ -1,9 +1,11 @@
-
 public class demo1 {
+    public int minus(int a, int b) {
+        return a - b;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        demo1 calc = new demo1();
+        int result = calc.minus(10, 4);
+        System.out.println("Result: " + result); // Output: Result: 6
+    }
 }
